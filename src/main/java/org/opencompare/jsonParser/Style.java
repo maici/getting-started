69e84e897eade83475be07c6391796e7c188bc9d
Mahdi <aici.mahdi@gmail.com>
2015-11-12 09:40:48 +0100

    1er version class Style

--- src/main/java/org/opencompare/jsonParser/Style.java
+++ src/main/java/org/opencompare/jsonParser/Style.java
@@ -7,59 +7,40 @@ import java.util.HashMap;
  */
 public class Style {
 
-    private String backgroundColor;
-    private String color;
-    private String font;
-    private String fontStyle;
-    private int fontSize;
     private HashMap<String, String> style;
 
     public Style() {
     }
 
-    public String getBackgroundColor() {
-        return backgroundColor;
-    }
-
     public void setBackgroundColor(String backgroundColor) {
-        this.backgroundColor = backgroundColor;
-        this.style.put("background-color", backgroundColor);
-    }
-
-    public String getColor() {
-        return color;
+        if(backgroundColor.matches("-?[0-9a-fA-F]+"))
+            this.style.put("background-color", "#" + backgroundColor);
+        else throw new NumberFormatException("background color must be in hexadecimal format");
     }
 
     public void setColor(String color) {
-        this.color = color;
-        this.style.put("color", color);
-    }
-
-    public String getFont() {
-        return font;
+        if(color.matches("-?[0-9a-fA-F]+"))
+            this.style.put("color", "#" + color);
+        else throw new NumberFormatException("color must be in hexadecimal format");
     }
 
     public void setFont(String font) {
-        this.font = font;
         this.style.put("font-family", font);
     }
 
-    public String getFontStyle() {
-        return fontStyle;
-    }
-
     public void setFontStyle(String fontStyle) {
-        this.fontStyle = fontStyle;
         if(fontStyle == "bold") this.style.put("font-weight", fontStyle);
         else if (fontStyle == "italic") this.style.put("font-style", fontStyle);
         else this.style.put("font-weight", "normal");
     }
 
-    public int getFontSize() {
-        return fontSize;
+    public void setFontSize(int fontSize) {
+        if(fontSize > 0)
+            this.style.put("font-size", Integer.toString(fontSize));
+        else throw new IllegalArgumentException("font size must be positive.");
     }
 
-    public void setFontSize(int fontSize) {
-        this.fontSize = fontSize;
+    public HashMap<String, String> getStyle() {
+        return style;
     }
 }
