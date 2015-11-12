2192a4c3fe68c2d268e56f3f8bc4284bf67f5ad1
Florian <Florian@148.60.14.205>
2015-11-12 09:19:27 +0100

    Squelette package cssGenerator

---
+++ src/main/java/org/opencompare/cssGenerator/PcmCssModule.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.opencompare.cssGenerator;
+
+import com.projetloki.genesis.CssBuilder;
+import com.projetloki.genesis.CssModule;
+import java.io.IOException;
+
+/**
+ *
+ * @author Florian
+ */
+public class PcmCssModule implements CssModule {
+
+    @Override
+    public void configure(CssBuilder cb) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
